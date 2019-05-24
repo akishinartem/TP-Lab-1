86df2a22289430596207133186c611f10b2fe2e2
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-05-24 13:22:20 +0300

    Add files via upload

---
+++ q_protect.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include "q_protect.h"
+using namespace std;
+
+void q_protect::calc() {
+	float sum = 0;
+	float sum_A = 0;
+	elem*temp = get_b();
+
+	while (temp != NULL) {
+		sum_A = sum_A + temp->Val;
+		temp = temp->Prev;
+	}
+	sum = sum_A / get_k();
+	cout << "Srednee arifmetich = " << sum << endl;
+}
+
+elem* q_protect::get_a() {
+	return q_A::get_a();
+}
+
+elem* q_protect::get_b() {
+	return q_A::get_b();
+}
+
+int q_protect::get_k() {
+	return q_A::get_k();
+}
+
+void q_protect::set_a(elem*value) {
+	q_A::set_a(value);
+}
+
+void q_protect::set_b(elem*value) {
+	q_A::set_b(value);
+}
+
+void q_protect::copy_queue(q_protect &op1) {
+	elem*temp = get_b();
+	int *mas = new int[get_k()];
+	int k = get_k() - 1;
+	for (int i = 0; i < get_k(); ++i)
+	{
+		mas[k--] = temp->Val;
+		temp = temp->Prev;
+	}
+	int n = get_k();
+	for (k = 0; k < n; k++)
+	{
+		op1.add(mas[k]);
+	}
+	delete[] mas;
+}
+
+q_protect* q_protect::merge(q_protect *op1) {
+	q_protect *res = new q_protect;
+	elem *temp = get_b();
+	int num = get_k();
+	int *mas_1 = new int[num];
+	int *mas_2 = new int[num];
+	int k = num - 1;
+	for (int i = 0; i < num; ++i)
+	{
+		mas_1[k--] = temp->Val;
+		temp = temp->Prev;
+	}
+	temp = op1->get_b();
+	k = num - 1;
+	for (int i = 0; i < num; ++i)
+	{
+		mas_2[k--] = temp->Val;
+		temp = temp->Prev;
+	}
+	for (int i = 0; i < num; ++i)
+		res->add(mas_1[i]);
+	for (int i = 0; i < num; ++i)
+		res->add(mas_2[i]);
+	delete[] mas_1;
+	delete[] mas_2;
+	mas_1 = nullptr;
+	mas_2 = nullptr;
+	return res;
+}
+
+void q_protect::print_queue() {
+	elem *temp = get_b();
+	int *mas = new int[get_k()];
+	int k = get_k() - 1;
+	for (int i = 0; i < get_k(); ++i)
+	{
+		mas[k--] = temp->Val;
+		temp = temp->Prev;
+	}
+	for (int i = 0; i < get_k(); ++i)
+	{
+		i != get_k() - 1 ? cout << mas[i] << " << " : cout << mas[i] << endl;
+	}
+	delete[]  mas;
+}
--- q_protect.h
+++ q_protect.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "q_A.h"
+
+class q_protect : protected q_A {
+public:
+	void add(int user_value) { q_A::add(user_value); }
+	void del() { q_A::del(); }
+	void print_queue();
+	void calc();
+	void copy_queue(q_protect &op1);
+	q_protect *merge(q_protect *op1);
+	void set_a(elem*value);
+	void set_b(elem*value);
+	elem* get_a();
+	elem* get_b();
+	int get_k();
+};
