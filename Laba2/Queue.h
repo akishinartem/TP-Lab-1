7feffca77ce4d4726623c255d1f0afff52cf0f1a
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-05-31 21:28:48 +0300

    Add files via upload

---
+++ Laba2/Queue.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include "elem.h"
+
+class Queue {
+	elem *Head;
+	elem *Tail;
+	int k;
+public:
+	Queue();
+	~Queue();
+
+	int GetK();
+	elem* GetHead();
+	elem* GetTail();
+	void SetHead(elem *Val);
+	void SetTail(elem *Val);
+
+	void Add();
+	void Del();
+	void Print();
+	void Copy(Queue &op1);
+
+	Queue& operator!();
+	Queue& operator++();
+	Queue& operator--(int);
+
+	friend Queue& operator++(Queue &op1, int usrVal);
+	friend Queue& operator--(Queue &op1);
+};
