3caa5fc5c8c0cb288e91e328e3943f129c7c8368
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-06-14 19:37:09 +0300

    заголовочный файл наследника public

--- Laba1/q_publ.h
+++ Laba1/q_publ.h
@@ -3,15 +3,7 @@
 #include "q_A.h"
 class q_publ : public q_A {
 public:
-	void add(int user_value) { q_A::add(user_value); }
-	void del() { q_A::del(); }
-	void print_queue();
 	void calc();
-	void copy_queue(q_publ &op1);
-	q_publ *merge(q_publ *op1);
-	void set_a(elem*value);
-	void set_b(elem*value);
-	elem* get_a();
-	elem* get_b();
-	int get_k();
+	void new_a(elem*value);
+	void new_b(elem*value);
 };
