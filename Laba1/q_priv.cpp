f9f41676a68e5f1668b6550c3144d549ac518214
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-06-14 19:39:05 +0300

    функции наследника private

--- Laba1/q_priv.cpp
+++ Laba1/q_priv.cpp
@@ -15,86 +15,10 @@ void q_priv::calc() {
 	cout << "Среднее арифметическое = [" << sum << "]\n";
 }
 
-elem* q_priv::get_a() {
-	return q_A::get_a();
+void q_priv::new_a(elem*value) {
+	set_a(value);
 }
 
-elem* q_priv::get_b() {
-	return q_A::get_b();
-}
-
-int q_priv::get_k() {
-	return q_A::get_k();
-}
-
-void q_priv::set_a(elem*value) {
-	q_A::set_a(value);
-}
-
-void q_priv::set_b(elem*value) {
-	q_A::set_b(value);
-}
-
-void q_priv::copy_queue(q_priv &op1) {
-	elem*temp = get_b();
-	int *mas = new int[get_k()];
-	int k = get_k() - 1;
-	for (int i = 0; i < get_k(); ++i)
-	{
-		mas[k--] = temp->Val;
-		temp = temp->Prev;
-	}
-	int n = get_k();
-	for (k = 0; k < n; k++)
-	{
-		op1.add(mas[k]);
-	}
-	delete[] mas;
-}
-
-q_priv* q_priv::merge(q_priv *op1) {
-	q_priv *res = new q_priv;
-	elem *temp = get_b();
-	int num = get_k();
-	int *mas_1 = new int[num];
-	int *mas_2 = new int[num];
-	int k = num - 1;
-	for (int i = 0; i < num; ++i)
-	{
-		mas_1[k--] = temp->Val;
-		temp = temp->Prev;
-	}
-	temp = op1->get_b();
-	k = num - 1;
-	for (int i = 0; i < num; ++i)
-	{
-		mas_2[k--] = temp->Val;
-		temp = temp->Prev;
-	}
-	for (int i = 0; i < num; ++i)
-		res->add(mas_1[i]);
-	for (int i = 0; i < num; ++i)
-		res->add(mas_2[i]);
-	delete[] mas_1;
-	delete[] mas_2;
-	mas_1 = nullptr;
-	mas_2 = nullptr;
-	return res;
-}
-
-void q_priv::print_queue() {
-	elem *temp = get_b();
-	int *mas = new int[get_k()];
-	int k = 0;
-	k = get_k() - 1;
-	for (int i = 0; i < get_k(); ++i)
-	{
-		mas[k--] = temp->Val;
-		temp = temp->Prev;
-	}
-	for (int i = 0; i < get_k(); ++i)
-	{
-		i != get_k() - 1 ? cout << "[" <<mas[i] << "], " : cout << mas[i] << "]\n";
-	}
-	delete[]  mas;
+void q_priv::new_b(elem*value) {
+	set_b(value);
 }
