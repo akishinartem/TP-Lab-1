eb9beb7b1323217f9b6ad5b1f772d43194384c2c
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-06-15 11:51:26 +0300

    заголовочный файл круга

---
+++ Laba3/Circle.h
@@ -0,0 +1,16 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#include "Area.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Circle: public Area {
+	double S;
+	double r;
+public:
+	Circle();
+	~Circle();
+	double Calc() override;
+};
