85fbb4e59b046523719ab9f28f56b28adc4e3c79
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-06-15 17:26:08 +0300

    узел элемента

--- Laba2/elem.h
+++ Laba2/elem.h
@@ -4,7 +4,7 @@ typedef struct elem {
 	int Val;
 	elem *Prev;
 public:
-	elem() :Val(0), Prev(nullptr) {};
+	elem() :Val(0), Prev(nullptr) { };
 	void setValue(int userValue = 0) { Val = userValue; }
 	int getValue() { return Val; }
 	void setNext(elem *Value) { Prev = Value; }
