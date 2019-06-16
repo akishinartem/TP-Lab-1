cbd324526c3e764cbf683d546286f31d17de7e0e
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-06-17 02:33:36 +0300

    функции родительского класса

--- Laba1/q_A.cpp
+++ Laba1/q_A.cpp
@@ -77,6 +77,18 @@ void q_A::print_queue() {
 	delete[] och;
 }
 
+int q_A::calc() {
+	float sum = 0;
+	float sum_A = 0;
+	elem*temp = get_b();
+	while (temp != NULL) {
+		sum_A = sum_A + temp->Val;
+		temp = temp->Prev;
+	}
+	sum = sum_A / get_k();
+	cout << "\nСреднее арифметическое = [" << sum << "]\n";
+}
+
 void q_A::copy_queue(q_A &op1) {
 	elem *temp = b;
 	int *och = new int[k];
