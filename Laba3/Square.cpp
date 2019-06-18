87f1639a152f507811e5bde21a32a3787d1d6ad4
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-06-18 17:38:56 +0300

    файл реализации квадрата

--- Laba3/Square.cpp
+++ Laba3/Square.cpp
@@ -3,12 +3,12 @@
 Square::Square() {
 	ifstream in("Square.txt");
 	int c;
-	cout << "\nВыбрать откуда будет считывание данных: " << endl;
+	cout << "\nРежим ввода данных: " << endl;
 	cout << "[1] С клавиатуры." << endl;
 	cout << "[2] Из файла." << endl;
-	cout << ">>";
+	cout << ">> ";
 	cin >> c;
-	system("cls");
+	cout << "\n";
 	if (c == 2) {
 		if (!in || FileCheck(in)) {
 			cout << "Файл пуст или не существует." << endl;
@@ -18,11 +18,10 @@ Square::Square() {
 		else {
 			in >> a;
 			cout << "Загруженные данные:" << endl;
-			cout << "Сторона: [" << a << "]." << endl;
+			cout << "Сторона: r = " << a << "." << endl;
 		}
 	}
 	if (c == 1) {
-		system("cls");
 		cout << "Введите сторону:\na = ";
 		cin >> a;
 	}
@@ -36,6 +35,6 @@ Square::~Square() {
 
 double Square::Calc() {
 	S = a * a;
-	cout << "Результат:\nS = ";
+	cout << "\nРезультат:\nS = ";
 	return S;
 }
