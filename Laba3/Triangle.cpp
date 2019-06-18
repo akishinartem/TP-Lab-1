e2e28d7b4ade705cb45793f6ddf672bb5f7c0ed0
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-06-18 17:44:55 +0300

    файл реализации прямоугольного треугольника

--- Laba3/Triangle.cpp
+++ Laba3/Triangle.cpp
@@ -3,12 +3,12 @@
 Triangle::Triangle() {
 	ifstream in("Triangle.txt");
 	int c;
-	cout << "\nВыбрать откуда будет считывание данных: " << endl;
+	cout << "\nРежим ввода данных: " << endl;
 	cout << "[1] С клавиатуры." << endl;
 	cout << "[2] Из файла" << endl;
-	cout << ">>";
+	cout << ">> ";
 	cin >> c;
-	system("cls");
+	cout << "\n";
 	if (c == 2) {
 		if (!in || FileCheck(in)) {
 			cout << "Файл пуст или не существует." << endl;
@@ -18,11 +18,10 @@ Triangle::Triangle() {
 		else {
 			in >> a >> b;
 			cout << "Загруженные данные:" << endl;
-			cout << "Стороны: [" << a << "], [" << b << "]." << endl;
+			cout << "Стороны: a = " << a << "; b = " << b << "." << endl;
 		}
 	}
 	if (c == 1) {
-		system("cls");
 		cout << "Введите стороны:\na = ";
 		cin >> a;
 		cout << "b = ";
@@ -39,6 +38,6 @@ Triangle::~Triangle() {
 
 double Triangle::Calc() {
 	S = (a*b) / 2.0;
-	cout << "Результат:\nS = ";
+	cout << "\nРезультат:\nS = ";
 	return S;
 }
