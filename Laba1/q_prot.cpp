e3ab42904204e6ea37e875c119fa98ed5aacd218
akishinartem <43314978+akishinartem@users.noreply.github.com>
2019-06-18 15:16:08 +0300

    функции наследника protected

--- Laba1/q_prot.cpp
+++ Laba1/q_prot.cpp
@@ -12,7 +12,30 @@ void q_prot::calc() {
 		temp = temp->Prev;
 	}
 	sum = sum_A / get_k();
-	cout << "Среднее арифметическое = [" << sum << "]\n";
+	cout << "\nСреднее арифметическое = [" << sum << "]\n";
+}
+
+void q_prot::print() {
+	if (!get_k()) {
+		cout << "Очередь пуста!" << endl;
+		return;
+	}
+	elem *temp = get_b();
+	int *och = new int[get_k()];
+	int l = get_k() - 1;
+	for (int i = 0; i < get_k(); ++i) {
+		och[l--] = temp->Val;
+		temp = temp->Prev;
+	}
+	cout << "Текущая очередь:" << endl;
+	for (int i = 0; i < get_k(); ++i) {
+		cout << "[" << och[i] << "] ";
+	}
+	delete[] och;
+}
+
+void q_prot::new_k(int num) {
+	set_k(num);
 }
 
 void q_prot::new_a(elem*value) {
